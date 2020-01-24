bf0f3720927ee4a03965b7ed210caffedd12502d
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-01-24 14:45:24 +0900

    Not Cha ge Activity

--- app/src/main/java/com/gardenlab/sampleorientationconfig/MainActivity.java
+++ app/src/main/java/com/gardenlab/sampleorientationconfig/MainActivity.java
@@ -1,8 +1,11 @@
 package com.gardenlab.sampleorientationconfig;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        if(newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            showToast("방향 : ORENTATION_LANDSCAPE");
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            showToast("방향 : ORIENTATION_PORTRAIT");
+        }
+    }
+
+    public void showToast(String data) {
+        Toast.makeText(this, data, Toast.LENGTH_SHORT).show();
     }
 }
